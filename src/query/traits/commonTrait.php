df9f3ee818042d710e7b61261e529deccfc5783d
sam.shi <32320811@qq.com>
2021-09-15 16:51:19 +0800

    bug修复

--- src/query/traits/commonTrait.php
+++ src/query/traits/commonTrait.php
@@ -125,26 +125,24 @@ trait commonTrait
             $bindParams = $model->getBindParams();
             return $this->$action($sql, $bindParams);
         } catch (\Exception $e) {
-            if (($e instanceof \yii\db\Exception) == true) {
-                $offset_1         = stripos($e->getMessage(), 'MySQL server has gone away');
-                $offset_2         = stripos($e->getMessage(), 'Lost connection to MySQL server');
-                $offset_3         = stripos($e->getMessage(), 'Error while sending QUERY packet');
-                $mysql_error_list = [
-                    2006,//MySQL server has gone away
-                    2013,//Lost connection to MySQL server
-                    1040,//已到达数据库的最大连接数，请加大数据库可用连接数
-                    1043,//无效连接
-                    1081,//不能建立Socket连接
-                    1158,//网络错误，出现读错误，请检查网络连接状况
-                    1159,//网络错误，读超时，请检查网络连接状况
-                    1160,//网络错误，出现写错误，请检查网络连接状况
-                    1161,//网络错误，写超时，请检查网络连接状况
-                    1203,//当前用户和数据库建立的连接已到达数据库的最大连接数，请增大可用的数据库连接数或重启数据库
-                    1205,//加锁超时
-                ];
-                if ($offset_1 || $offset_2 || $offset_3 || in_array($e->errorInfo[1], $mysql_error_list)) {
-                    return $this->reConnect()->$action($sql, $bindParams);
-                }
+            $offset_1         = stripos($e->getMessage(), 'MySQL server has gone away');
+            $offset_2         = stripos($e->getMessage(), 'Lost connection to MySQL server');
+            $offset_3         = stripos($e->getMessage(), 'Error while sending QUERY packet');
+            $mysql_error_list = [
+                2006,//MySQL server has gone away
+                2013,//Lost connection to MySQL server
+                1040,//已到达数据库的最大连接数，请加大数据库可用连接数
+                1043,//无效连接
+                1081,//不能建立Socket连接
+                1158,//网络错误，出现读错误，请检查网络连接状况
+                1159,//网络错误，读超时，请检查网络连接状况
+                1160,//网络错误，出现写错误，请检查网络连接状况
+                1161,//网络错误，写超时，请检查网络连接状况
+                1203,//当前用户和数据库建立的连接已到达数据库的最大连接数，请增大可用的数据库连接数或重启数据库
+                1205,//加锁超时
+            ];
+            if ($offset_1 || $offset_2 || $offset_3 || in_array($e->errorInfo[1], $mysql_error_list)) {
+                return $this->reConnect()->$action($sql, $bindParams);
             }
 
             throw $e;

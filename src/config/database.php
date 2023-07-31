027f43c865d1b4e48e04f8c68787b5689562f3c5
shixingyu <32320811@qq.com>
2023-07-31 17:25:31 +0800

    测试自动复制配置文件

---
+++ src/config/database.php
@@ -0,0 +1,49 @@
+<?php
+return [
+    'default' => [
+        [
+            'host'       => '127.0.0.1',
+            'port'       => 3306,
+            'username'   => 'root',
+            'password'   => '123456',
+            'database'   => 'test',
+            'charset'    => 'utf8',
+            'presistent' => false,
+            'type'       => 'master',
+            'debug'      => false,
+        ],
+        [
+            'host'       => '127.0.0.1',
+            'port'       => 3306,
+            'username'   => 'root',
+            'password'   => '123456',
+            'database'   => 'test',
+            'charset'    => 'utf8',
+            'presistent' => false,
+            'type'       => 'slave',
+            'debug'      => false,
+        ],
+    ],
+    'log'     => [
+        [
+            'host'       => '127.0.0.1',
+            'port'       => 3306,
+            'username'   => 'root',
+            'password'   => '123456',
+            'database'   => 'test',
+            'charset'    => 'utf8',
+            'presistent' => false,
+            'type'       => 'master',
+        ],
+        [
+            'host'       => '127.0.0.1',
+            'port'       => 3306,
+            'username'   => 'root',
+            'password'   => '123456',
+            'database'   => 'test',
+            'charset'    => 'utf8',
+            'presistent' => false,
+            'type'       => 'slave',
+        ],
+    ],
+];
